230439c1bb50ef52520d1d29dae6bed5ea936578
Samuelhdz <samuelhdz47@gmail.com>
2025-03-07 17:33:47 -0600

    attached3

---
+++ ProyectoV/IbaMex-master/src/components/index.ts
@@ -0,0 +1,4 @@
+
+export { default as RouteManager } from './RouteManager';
+export { default as DataAnalytics } from './DataAnalytics';
+export { default as MapRoute } from './MapRoute';
---
+++ ProyectoV/IbaMex-master/src/styles/index.ts
@@ -0,0 +1,3 @@
+
+import './RouteManager.css';
+import './DataAnalytics.css';
